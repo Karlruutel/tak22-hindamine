2fa7588fddf8a04a712110a5f8dd96c479d6d95a
karlruutel <karl.ruutel@ametikool.ee>
2023-04-28 09:58:57 +0300

    catt

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ const catsContainer = document.getElementById('container');
 heading.innerText = 'Karl ja kassid';
 
 fetch('https://catfact.ninja/breeds?limit=100')
-.then(res => res.json())
-.then(cats => {
+  .then(res => res.json())
+  .then(cats => {
 
-    cats.data.forEach( cat => {
+    cats.data.forEach(cat => {
 
-        const catDiv = document.createElement('div');
+      const catDiv = document.createElement('div');
 
-        catDiv.classList.add('cat');
+      catDiv.classList.add('cat');
 
-        catDiv.innerHTML = `
-            <div>${cat.breed}</div>
-            <div>${cat.country}</div>
-        `;
+      catDiv.innerHTML = `
 
-        catsContainer.append(catDiv);
-       
+        <div><strong>${cat.breed}</strong></div>
+        <div>${cat.country}</div>
+      `;
+
+      catDiv.style.border = 'none'; // remove border
+
+      catsContainer.append(catDiv);
+      catDiv.style.margin = '5px';
+
     });
-});
+  });
